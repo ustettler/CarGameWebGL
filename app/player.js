d614f426b0483ff1eaa2576838cec1f6ecccd4d6
Urs Stettler <boundlessenjoy@gmail.com>
2022-02-07 20:29:57 +0100

    fix: 🐛 Markdown II

--- app/player.js
+++ app/player.js
@@ -1,45 +1,51 @@
 /// <reference path="../typings/node_modules/@types/jquery/index.d.ts" />
 var RacingGame;
 (function (RacingGame) {
-    class Player {
-        constructor(pManager) {
-            this.manager = pManager;
-            this.reset();
-        }
-        reset() {
-            this.speed = 0;
-            this.speedChanges = 0;
-            if (this.refPlayerModel !== undefined) {
-                this.refPlayerModel.position.x = 0;
-                this.refPlayerModel.position.z = 0;
-            }
-        }
-        switchLeftRightPlayer(pSwitchValue) {
-            if (pSwitchValue < 0 && this.refPlayerModel.position.x >= 0) {
-                this.refPlayerModel.position.x -= 5.5;
-            }
-            else if (pSwitchValue > 0 && this.refPlayerModel.position.x <= 0) {
-                this.refPlayerModel.position.x += 5.5;
-            }
-        }
-        move() {
-            if (this.speedChanges !== 0) {
-                this.speed += this.speedChanges;
-                if (this.speed <= 0) {
-                    this.speed = 0;
-                    this.speedChanges = 0;
-                }
-            }
-            this.refPlayerModel.position.z -= (this.speed * 0.01);
-            this.manager.engine.cameraGroup.position.z = this.refPlayerModel.position.z;
-            $("#speedData").html("" + Math.floor(this.speed));
-            if (this.refPlayerModel.position.z < -1110 && this.manager.gameState === RacingGame.GameState.Running) {
-                this.manager.gameState = RacingGame.GameState.Finish;
-                this.speedChanges = -12;
-            }
-            //Collision Detection
-        }
+  class Player {
+    constructor(pManager) {
+      this.manager = pManager;
+      this.reset();
     }
-    RacingGame.Player = Player;
+    reset() {
+      this.speed = 0;
+      this.speedChanges = 0;
+      if (this.refPlayerModel !== undefined) {
+        this.refPlayerModel.position.x = 0;
+        this.refPlayerModel.position.z = 0;
+      }
+    }
+    switchLeftRightPlayer(pSwitchValue) {
+      if (pSwitchValue < 0 && this.refPlayerModel.position.x >= 0) {
+        this.refPlayerModel.position.x -= 5.5;
+      } else if (pSwitchValue > 0 && this.refPlayerModel.position.x <= 0) {
+        this.refPlayerModel.position.x += 5.5;
+      }
+    }
+    move() {
+      if (this.speedChanges !== 0) {
+        this.speed += this.speedChanges;
+        if (this.speed <= 0) {
+          this.speed = 0;
+          this.speedChanges = 0;
+        }
+      }
+      this.refPlayerModel.position.z -= this.speed * 0.01;
+      this.manager.engine.cameraGroup.position.z =
+        this.refPlayerModel.position.z;
+      $("#speedData").html("" + Math.floor(this.speed));
+      if (
+        this.refPlayerModel.position.z < -1110 &&
+        this.manager.gameState === RacingGame.GameState.Running
+      ) {
+        this.manager.gameState = RacingGame.GameState.Finish;
+        this.speedChanges = -12;
+      }
+      //Collision Detection
+      for (let i = 0; i < this.manager.level.dataMap.length; i++) {
+        let TempObj = this.manager.level.dataMap[i];
+      }
+    }
+  }
+  RacingGame.Player = Player;
 })(RacingGame || (RacingGame = {}));
 //# sourceMappingURL=player.js.map
